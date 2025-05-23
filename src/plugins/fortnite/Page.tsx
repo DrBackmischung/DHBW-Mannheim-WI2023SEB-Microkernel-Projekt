8ab8f2f9f98db39a3c304f3ce19f75adab998f26
Max Meinel <max.meinel@gmx.net>
2025-05-23 17:17:36 +0200

    Update Fortnite plugin page to include new GIFs

--- src/plugins/fortnite/Page.tsx
+++ src/plugins/fortnite/Page.tsx
@@ -1,6 +1,7 @@
 export const FortnitePluginPage: React.FC = () => (
     <div>
       <h2>Fortnite!</h2>
-      <p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUziiHJwIcyxLP9szl22WLH3ax28PnxocQdg&s" alt="" /></p>
+      <img src="https://media.tenor.com/U-G2j_J2xnAAAAAM/peely-fortnite.gif" alt="" />
+      <img src="https://media.tenor.com/gjbq2A5lzDcAAAAM/fortnite-fortnite-dance.gif" alt="" />
     </div>
   );
